da19fe0ad83a2c459e9507bb930fb01fdc30abe9
Priyatam <priyatamkumar0398@gmail.com>
2025-08-06 15:27:20 -0400

    Just git pulled and commit

--- galmorph/core.py
+++ galmorph/core.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 df_loaded = pd.read_pickle("morphologies_snapshot_data.pkl")
 # df_loaded = pd.read_csv("galaxies_morph.csv")
-print(df_loaded.dtypes)
-print(df_loaded.columns)
+# print(df_loaded.dtypes)
+# print(df_loaded.columns)
 
 snapshot_values = df_loaded["Snapshot"].drop_duplicates().to_list()
 galaxies_count = []
 for val in snapshot_values:
-    print(val, ":", df_loaded[df_loaded["Snapshot"]==val].shape[0])
+    # print(val, ":", df_loaded[df_loaded["Snapshot"]==val].shape[0])
     galaxies_count.append(df_loaded[df_loaded["Snapshot"]==val].shape[0])
 
 plt.figure(figsize = [6, 4])
