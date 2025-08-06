355b4dcbe0d20f1f6e8f06df984514c1a60a8a14
impriyatam <priyatamkumar0398@gmail.com>
2025-08-06 10:54:35 -0400

    structure the code into package format && rename the earlier the core.py to data_conversion.py

---
+++ galmorph/core.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df_loaded = pd.read_pickle("morphologies_snapshot_data.pkl")
+# df_loaded = pd.read_csv("galaxies_morph.csv")
+print(df_loaded.dtypes)
+print(df_loaded.columns)
+
+snapshot_values = df_loaded["Snapshot"].drop_duplicates().to_list()
+galaxies_count = []
+for val in snapshot_values:
+    print(val, ":", df_loaded[df_loaded["Snapshot"]==val].shape[0])
+    galaxies_count.append(df_loaded[df_loaded["Snapshot"]==val].shape[0])
+
+plt.figure(figsize = [6, 4])
+plt.plot(snapshot_values, galaxies_count)
+plt.show()
