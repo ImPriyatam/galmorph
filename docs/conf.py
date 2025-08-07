6050f1f696ee3479df82010c7136ab2360567cd2
shahddmo <shahdmoghazy17@gmail.com>
2025-08-08 01:43:46 +0300

    editing in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,19 +3,13 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../galmorph/'))
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'GalMorph'
 copyright = '2025, Priyatam Kumar, Shahd Moghazy and Mitali Damle'
 author = 'Priyatam Kumar, Shahd Moghazy and Mitali Damle'
-root_doc = 'index'
-
-release = 'v0.1'
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
